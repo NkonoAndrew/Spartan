d8121208112fe0d92228e85bbcf06fc6863badb7
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-03-31 12:02:39 -0700

    Initial Commit

---
+++ SpartanNet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpartanNet
+//
+//  Created by Bella Wei on 3/31/20.
+//  Copyright © 2020 Bella Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
