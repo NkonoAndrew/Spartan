6991d0b2b116dae223a82ff1aa51ebbd3f5cfacb
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-04-02 21:11:36 -0700

    initial checkin

---
+++ SpartanNet/Pods/Target Support Files/Pods-SpartanNet/Pods-SpartanNet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpartanNet : NSObject
+@end
+@implementation PodsDummy_Pods_SpartanNet
+@end
---
+++ SpartanNet/SpartanNet/Controller/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SpartanNet
+//
+//  Created by Bella Wei on 3/31/20.
+//  Copyright © 2020 Bella Wei. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class ViewController: UIViewController, GIDSignInDelegate{
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+    
+    if let error = error {
+        print("\(error.localizedDescription)")
+    } else {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let tabbarVC = storyboard.instantiateViewController(withIdentifier: "TabBarIdentifier") as! UITabBarController
+        self.present(tabbarVC, animated: false, completion: nil)
+    }
+    }
+
+    @IBOutlet weak var GidSigninButton: GIDSignInButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        GIDSignIn.sharedInstance().presentingViewController = self
+        GIDSignIn.sharedInstance()?.signIn()
+        GIDSignIn.sharedInstance()?.delegate = self
+    }
+    
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!,
+  withError error: NSError!) {
+    if (error == nil) {
+      // Perform any operations on signed in user here.
+      // ...
+    } else {
+      print("\(error.localizedDescription)")
+    }
+}
+
+
+}
+
