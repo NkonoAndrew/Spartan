8fb059125d4a7e45ea48216de7b9009fd7f3dd0e
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-04-14 10:22:26 -0700

    add UITableview

---
+++ SpartanNet/SpartanNet/View/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  SpartanNet
+//
+//  Created by Bella Wei on 4/13/20.
+//  Copyright © 2020 Bella Wei. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SpartanNet/SpartanNet/View/postModel.swift
@@ -0,0 +1,9 @@
+//
+//  postModel.swift
+//  SpartanNet
+//
+//  Created by Bella Wei on 4/13/20.
+//  Copyright © 2020 Bella Wei. All rights reserved.
+//
+
+import Foundation
